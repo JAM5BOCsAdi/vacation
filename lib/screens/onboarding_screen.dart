986e4102e0a111a699d9014b5afb7a5bb051e441
CsAdi <oradam86@gmail.com>
2023-09-22 22:29:41 +0200

    OnBoardingScreen

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -35,46 +35,68 @@ class OnboardingScreen extends StatelessWidget {
                   width: width,
                 ),
               ),
-              Text(
-                kOnBoardingTitle,
-                style: GoogleFonts.plusJakartaSans(
-                  textStyle: const TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w700,
-                    height: 1.3333333333,
-                    letterSpacing: 0.12,
-                    color: kBlackColor,
-                  ),
-                ),
-              ),
-              Text(
-                kOnBoardingDescription,
-                style: GoogleFonts.plusJakartaSans(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
-                  height: 1.5714285714,
-                  letterSpacing: 0.07,
-                  color: kGreyColor,
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text(kOnBoardingButton),
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 0, right: 4, left: 0, bottom: 0),
-                    child: Text(
-                      kOnBoardingDHA,
-                      style: textStyle(kBlackColor),
+              Padding(
+                padding: const EdgeInsets.only(left: 24, top: 0, right: 24, bottom: 0),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15.5, top: 0, right: 15.5, bottom: 33),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 9),
+                            child: Text(
+                              kOnBoardingTitle,
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.plusJakartaSans(
+                                textStyle: const TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w700,
+                                  height: 1.33,
+                                  letterSpacing: 0.12,
+                                  color: kBlackColor,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Text(
+                            kOnBoardingDescription,
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.plusJakartaSans(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                              height: 1.57,
+                              letterSpacing: 0.07,
+                              color: kGreyColor,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  Text(
-                    kOnBoardingRegister,
-                    style: textStyle(kGreenColor),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.only(top: 0, right: 0, left: 0, bottom: 24),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text(kOnBoardingButton),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 0, right: 4, left: 0, bottom: 0),
+                          child: Text(
+                            kOnBoardingDHA,
+                            style: textStyle(kBlackColor),
+                          ),
+                        ),
+                        Text(
+                          kOnBoardingRegister,
+                          style: textStyle(kGreenColor),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
